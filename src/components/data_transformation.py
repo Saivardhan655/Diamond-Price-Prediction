e7313094f40a393c30966d9b6bb60726fb3e445b
Saivardhan655 <saivardhan9154@gmail.com>
2024-07-15 23:42:15 +0530

    Complete

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,6 +1,6 @@
-from sklearn.impute import SimpleImputer ## HAndling Missing Values
-from sklearn.preprocessing import StandardScaler # HAndling Feature Scaling
-from sklearn.preprocessing import OrdinalEncoder # Ordinal Encoding
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OrdinalEncoder 
 ## pipelines
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
@@ -16,15 +16,10 @@ from src.logger import logging
 from src.utils import save_object
 
 
-## Data Transformation config
-
 @dataclass
 class DataTransformationconfig:
     preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
 
-
-
-## Data Ingestionconfig class
 
 class DataTransformation:
     def __init__(self):
@@ -34,18 +29,16 @@ class DataTransformation:
          
          try:
             logging.info('Data Transformation initiated')
-            # Define which columns should be ordinal-encoded and which should be scaled
             categorical_cols = ['cut', 'color','clarity']
             numerical_cols = ['carat', 'depth','table', 'x', 'y', 'z']
-            
-            # Define the custom ranking for each ordinal variable
+
             cut_categories = ['Fair', 'Good', 'Very Good','Premium','Ideal']
             color_categories = ['D', 'E', 'F', 'G', 'H', 'I', 'J']
             clarity_categories = ['I1','SI2','SI1','VS2','VS1','VVS2','VVS1','IF']
             
             logging.info('Pipeline Initiated')
 
-            ## Numerical Pipeline
+
             num_pipeline=Pipeline(
                 steps=[
                 ('imputer',SimpleImputer(strategy='median')),
@@ -55,7 +48,7 @@ class DataTransformation:
 
             )
 
-            # Categorigal Pipeline
+    
             cat_pipeline=Pipeline(
                 steps=[
                 ('imputer',SimpleImputer(strategy='most_frequent')),
@@ -83,7 +76,6 @@ class DataTransformation:
 
     def initiate_data_transformation(self,train_path,test_path):
         try:
-            # Reading train and test data
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
 
@@ -98,8 +90,7 @@ class DataTransformation:
             target_column_name = 'price'
             drop_columns = [target_column_name,'id']
 
-            ## features into independent and dependent features
-
+            
             input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
             target_feature_train_df=train_df[target_column_name]
 
@@ -107,7 +98,7 @@ class DataTransformation:
             input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
             target_feature_test_df=test_df[target_column_name]
 
-            ## apply the transformation
+    
 
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
